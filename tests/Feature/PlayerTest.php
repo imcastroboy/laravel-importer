79be2dc1dae94275ea7b62be7569e089840a8306
Jess Castro <gzecastro@gmail.com>
2019-12-18 20:29:53 +0800

    Implements test for all changes, specially in importing, displaying of data

--- tests/Feature/PlayerTest.php
+++ tests/Feature/PlayerTest.php
@@ -2,31 +2,94 @@
 
 namespace Tests\Feature;
 
-use App\Contracts\Importer;
-use App\Http\Resources\Player as PlayerResource;
-use App\Http\Resources\Players;
-use App\Player;
-use App\Stats;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use File;
 use Mockery;
+use App\Stats;
+use App\Player;
 use Tests\TestCase;
+use App\Contracts\Importer;
+use App\Http\Resources\Players;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Http\Resources\Player as PlayerResource;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class PlayerTest extends TestCase
 {
-
     use DatabaseMigrations;
 
-    /** @test */
-    public function it_will_save_players_and_count()
-    {
-        $totalPlayers = 5; //config('importer.limit');
+    private $importer;
 
-        $players = factory(Player::class, $totalPlayers)
-            ->create()->each(function ($player) {
-                $player->stats()->save(factory(Stats::class)->make());
-        });
+    private $source;
+
+    /**
+    * After the first run of setUp "migrate:fresh"
+    * @return void
+    */
+    public function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('migrate:fresh');
+        $this->source = base_path('tests/mocks/api.json');
+        $this->importer = $this->instance(\App\Contracts\Importer::class,
+            new \App\Services\JsonImporterService(
+                new \App\Services\NativeHttpClientService($this->source),
+                new \App\Repositories\PlayerRepository
+            )
+        );
+    }
+
+    private function import()
+    {
+        return $this->artisan('import:players');
+    }
+
+    /** @test */
+    public function it_will_save_data_from_an_api()
+    {
+        $this->import()->expectsOutput('Import Done!');
+    }
+
+    /** @test */
+    public function check_if_data_exists_in_the_database()
+    {
+        $this->import();
+
+        $repo = new \App\Repositories\PlayerRepository;
+
+        $player = $this->importer->transform()[0];
+        $stats = $repo->getStat($player);
+        $this->assertDatabaseHas('players', $repo->getPlayerInfo($player));
+        $this->assertDatabaseHas('stats', [
+            'form' => $player->form(),
+            'total_points' => $player->totalPoints(),
+            'influence' => $player->influence(),
+            'creativity' => $player->creativity(),
+            'threat' => $player->threat(),
+            'ict_index' => $player->ictIndex(),
+        ]);
+
+    }
+
+    /** @test */
+    public function check_if_output_is_instance_of_footballplayer_class()
+    {
+        $player = $this->importer->transform()[0];
+
+        $this->assertInstanceOf(\App\Entities\FootballPlayer::class, $player);
+        $this->assertInstanceOf(\App\Contracts\Person::class, $player);
+        $this->assertInstanceOf(\App\Contracts\Player::class, $player);
+        $this->assertInstanceOf(\App\Contracts\Stats::class, $player);
+    }
+
+    /** @test */
+    public function check_if_saved_players_is_100()
+    {
+        $this->import();
+
+        $totalPlayers = config('importer.limit');
 
         $numOfPlayers = Player::all();
 
@@ -37,10 +100,7 @@ class PlayerTest extends TestCase
     /** @test */
     public function it_will_show_all_players()
     {
-        $players = factory(Player::class, 5)->create()->each(function ($player) {
-            $player->stats()->save(factory(Stats::class)->make());
-        });
-
+        $this->import();
 
         $response = $this->json('GET', route('players.index'));
 
@@ -74,13 +134,11 @@ class PlayerTest extends TestCase
     /** @test */
     public function it_will_show_a_player()
     {
-        $players = factory(Player::class, 5)->create()->each(function ($player) {
-            $player->stats()->save(factory(Stats::class)->make());
-        });
+        $this->import();
 
         $player = new PlayerResource(Player::first());
 
-            $response = $this->json('GET', route('players.show', $player->id));
+        $response = $this->json('GET', route('players.show', $player->id));
 
         $response
             ->assertStatus(200)
